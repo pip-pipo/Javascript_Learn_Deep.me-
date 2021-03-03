7261cbc5f8903800c1e9cfb357c068add99ce279
pip-pipo <codemi692@gmail>
2021-03-02 20:53:25 -1100

    :fire:

--- day2/app.js
+++ day2/app.js
@@ -4,7 +4,6 @@
 // const dir = path.dirname(__dirname);
 // // let mkaeFile = fs.writeFileSync(dir+'index.html',"morsalin")
 
-
 // fs.open('index.html', 'r', (err, fd) => {
 //     if (err) throw err;
 //     fs.close(fd, (err) => {
@@ -12,5 +11,25 @@
 //     });
 //   });
 
-
 //  fs.readFileSync(dir+'index.html')
+
+const fun = (n) => {
+  return (n * n) / 2 - (1 + n);
+};
+console.log(fun(101));
+const data = fun(30).toString().slice(0,2)
+const intData = Number(data) * 1.0 / 1.01;
+
+
+console.log(intData)
+
+
+
+function random(){
+    let int = 0;
+    int++;
+    return int+=033/44*454/45/(3*4.000)
+}
+
+console.log(random())
+console.log(Math.random())
