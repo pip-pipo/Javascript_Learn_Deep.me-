0a07e2120009b78199937ab6106c771c8ad6bf25
pip-pipo <codemi692@gmail>
2021-03-11 21:30:59 -1100

    :fire:

--- day2/app.js
+++ day2/app.js
@@ -13,23 +13,45 @@
 
 //  fs.readFileSync(dir+'index.html')
 
-const fun = (n) => {
-  return (n * n) / 2 - (1 + n);
-};
-console.log(fun(101));
-const data = fun(30).toString().slice(0,2)
-const intData = Number(data) * 1.0 / 1.01;
+// const fun = (n) => {
+//   return (n * n) / 2 - (1 + n);
+// };
+// console.log(fun(101));
+// const data = fun(30).toString().slice(0,2)
+// const intData = Number(data) * 1.0 / 1.01;
 
 
-console.log(intData)
+// console.log(intData)
 
 
+// function random(){
+//     let int = 0;
+//     int++;
+//     return int+=033/44*454/45/(3*4.000)
+// }
 
-function random(){
-    let int = 0;
-    int++;
-    return int+=033/44*454/45/(3*4.000)
+// console.log(random())
+// console.log(Math.random())
+
+const random  = 'djkdsjfdjf';
+const fun=(text)=>{
+  let i = '';
+  for(let i = 0;i<text.length;i++){
+    i+=random.charAt(i).toString().substring();
+  }
+  return i ;
 }
+fun(random)
+console.log(fun(5))
+let morsalin = 'morsalin'
 
-console.log(random())
-console.log(Math.random())
+for(let i = 0;i<10;i++){
+
+  let flore = Math.floor(Math.random(1204)* Math.sqrt(4) * 7)
+  morsalin+=morsalin.charAt(flore).toString()
+  console.log(flore)
+}
+console.log(morsalin)
+
+const math = '54545';
+
